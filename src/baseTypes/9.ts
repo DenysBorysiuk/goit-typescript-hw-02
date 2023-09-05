314f255c647d98d603aa0e3de902eb3a6a4f9301
Denys Borysiuk <denisborisiuk@gmail.com>
2023-09-05 15:54:30 +0300

    add task 09

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -2,6 +2,19 @@
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
 
+type Details = {
+  createAt: object;
+  updateAt: object;
+};
+
+type Page = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: 'open' | 'close';
+  details?: Details;
+};
+
 const page1 = {
   title: 'The awesome page',
   likes: 100,
@@ -10,14 +23,14 @@ const page1 = {
   details: {
     createAt: new Date('2021-01-01'),
     updateAt: new Date('2021-05-01'),
-  }
-}
+  },
+};
 
-const page2 = {
+const page2: Page = {
   title: 'Python or Js',
   likes: 5,
   accounts: ['Alex'],
   status: 'close',
-}
+};
 
 export {};
